20545e048a6d962967b5eae15761fd724df90eee
Ozmar Mendoza <amfc11@gmail.com>
2022-12-15 21:20:27 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
 	reactStrictMode: true,
+	swcMinify: true,
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.svg$/,
+			issuer: /\.[jt]sx?$/,
+			use: ["@svgr/webpack"],
+		});
+
+		return config;
+	},
 };
