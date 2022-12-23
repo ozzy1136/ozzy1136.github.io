8ddab453962408611dc329abe584300614bd31d0
Ozmar Mendoza <amfc11@gmail.com>
2022-12-22 22:36:43 -0600

    Incorporate old Services code to Bio Section

--- components/Home/BioSection/index.jsx
+++ components/Home/BioSection/index.jsx
@@ -10,8 +10,8 @@ export default function HomeBioSection() {
 			<div id="bioHeading" className={`${styles.sectionHeadingWrapper}`}>
 				<h2 className={`${styles.sectionHeading}`}>
 					Build&shy;ing on the{" "}
-					<span className={`${styles.text_CPU}`}>web</span>, for{" "}
-					<span className={`${styles.text_CPU}`}>all</span>
+					<span className={`${styles.textEmphasized}`}>web</span>, for{" "}
+					<span className={`${styles.textEmphasized}`}>all</span>
 				</h2>
 			</div>
 			<div className={`${styles.bioWrapper}`}>
@@ -28,26 +28,11 @@ export default function HomeBioSection() {
 				</p>
 				<br />
 				<p>
-					<span className={`${styles.text_TLDR}`}>tl;dr:</span> I am a
+					<span className={`${styles.textTLDR}`}>tl;dr:</span> I am a
 					partner and collaborator for creatives and companies
 					aspiring to create engaging web experiences!
 				</p>
 			</div>
-			<h2 id="servicesHeading" className={`${styles.sectionHeading}`}>
-				Services
-			</h2>
-			<h3 className={`${styles.sectionSubHeading}`}>
-				Areas of work that I can help you with
-			</h3>
-			<p className={`${styles.servicesSummary}`}>
-				What are you doing then? We often hear. Do you also do websites?
-				Yes, also websites. Videos? Well, the story and concept, for the
-				production we hook up a partner. We take a holistic approach, so
-				we can mean a lot. Can&apos;t we do it ourselves? Then why
-				don&apos;t we ask someone else? We have an eye for it. According
-				to our own standard with quality first. We keep control
-				ourselves, which is nice.
-			</p>
 			<ul className={`${styles.servicesList} l-grid list-unmarked`}>
 				<li className={`${styles.serviceCategory}`}>
 					<div className={`${styles.serviceName}`}>
