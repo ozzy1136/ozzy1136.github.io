49019d9b0bcc20849a585e8033671c219989b761
Ozmar Mendoza <amfc11@gmail.com>
2022-12-22 20:27:18 -0600

    Move <ServicesSection/> code to <BioSection/>

--- components/Home/BioSection/index.jsx
+++ components/Home/BioSection/index.jsx
@@ -9,15 +9,10 @@ export default function HomeBioSection() {
 		>
 			<div id="bioHeading" className={`${styles.sectionHeadingWrapper}`}>
 				<h2 className={`${styles.sectionHeading}`}>
-					Build&shy;ing for the{" "}
-					<span className={`${styles.text_CPU}`}>
-						aver&shy;age CPU
-					</span>{" "}
+					Build&shy;ing on the{" "}
+					<span className={`${styles.text_CPU}`}>web</span>, for{" "}
+					<span className={`${styles.text_CPU}`}>all</span>
 				</h2>
-				<span className={`${styles.sectionHeadingAddendum}`}>
-					(<abbr title="For Example">e.g.</abbr> No para&shy;llax
-					effect or full page hero videos &#129335;)
-				</span>
 			</div>
 			<div className={`${styles.bioWrapper}`}>
 				<p>
@@ -38,6 +33,58 @@ export default function HomeBioSection() {
 					aspiring to create engaging web experiences!
 				</p>
 			</div>
+			<h2 id="servicesHeading" className={`${styles.sectionHeading}`}>
+				Services
+			</h2>
+			<h3 className={`${styles.sectionSubHeading}`}>
+				Areas of work that I can help you with
+			</h3>
+			<p className={`${styles.servicesSummary}`}>
+				What are you doing then? We often hear. Do you also do websites?
+				Yes, also websites. Videos? Well, the story and concept, for the
+				production we hook up a partner. We take a holistic approach, so
+				we can mean a lot. Can&apos;t we do it ourselves? Then why
+				don&apos;t we ask someone else? We have an eye for it. According
+				to our own standard with quality first. We keep control
+				ourselves, which is nice.
+			</p>
+			<ul className={`${styles.servicesList} l-grid list-unmarked`}>
+				<li className={`${styles.serviceCategory}`}>
+					<div className={`${styles.serviceName}`}>
+						Bespoke Website Development
+					</div>
+					<ol className={`${styles.serviceDetailsList}`}>
+						<li>E-commerce</li>
+						<li>Internal tools and dashboards</li>
+						<li>Web Applications</li>
+					</ol>
+				</li>
+				<li className={`${styles.serviceCategory}`}>
+					<div className={`${styles.serviceName}`}>
+						<abbr title="Content Management Systems">CMS</abbr> and
+						Webisite Builders
+					</div>
+					<ol className={`${styles.serviceDetailsList}`}>
+						<li>Wordpress</li>
+						<li>Shopify</li>
+						<li>Wix</li>
+						<li>Squarespace</li>
+						<li>Joomla</li>
+						<li>Drupal</li>
+					</ol>
+				</li>
+				<li className={`${styles.serviceCategory}`}>
+					<div className={`${styles.serviceName}`}>Consulting</div>
+					<ol className={`${styles.serviceDetailsList}`}>
+						<li>Accessibility</li>
+						<li>
+							<abbr title="User Experience">UX</abbr>
+						</li>
+						<li>Performance</li>
+						<li>Technology Strategy Analysis</li>
+					</ol>
+				</li>
+			</ul>
 		</section>
 	);
 }
